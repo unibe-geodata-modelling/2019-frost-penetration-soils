28b0a0193282cf7b085ae4a4f0cbf95f3f1b50a3
LukasGiub <48594787+LukasGiub@users.noreply.github.com>
2019-11-27 11:40:55 +0100

    Update makeGIF.py

--- makeGIF.py
+++ makeGIF.py
@@ -8,6 +8,15 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 # os.chdir('C:/Users/User/Documents/geodataanalysis/soliflu')  # set working directory manually
 
+# Choose elevation if it is not definded aleady
+try:
+    elevation
+except NameError:
+    elevation = input("choose an elevation! take 2400, 2500 or 2600")
+    print("OK")
+else:
+    print("elevation is already defined")
+
 # make list of all frames in "pics" folder. Replace "pics" by "pics2400", "pics2500" or "pics2600"
 frames = []
 imgs = glob.glob('pics' + str(elevation) + '/*.png')  #  (note: for me... for filename in glob.glob(('pics' + str(elevation) + '/*.png')):)
